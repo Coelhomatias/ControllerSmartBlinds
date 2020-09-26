710ed95099b0b0093ea64dcc2eaf6fa6e7b6b5e8
Coelhomatias <leandrohenrique.lf@gmail.com>
2020-09-26 20:43:19 +0100

    Added ResettableTimer to check availability

--- Controller.py
+++ Controller.py
@@ -1,5 +1,6 @@
 from MQTTComponent import MQTTComponent
 from skmultiflow.meta import AdaptiveRandomForestRegressor
+from resettabletimer import ResettableTimer
 import datetime as dt
 import time
 
@@ -7,15 +8,20 @@ import time
 class Controller():
 
     def __init__(
-            self, name, identifier, device, learning_time=dt.timedelta(days=7),
+            self, name, identifier, device, timeout=350, learning_time=dt.timedelta(days=7),
             model=AdaptiveRandomForestRegressor(random_state=43, n_estimators=100, grace_period=50, max_features=11, leaf_prediction='mean', split_confidence=0.09, lambda_value=10)):
 
         self._name = name
         self._identifier = identifier
         self._device = device
+        self._timeout = timeout #In seconds
         self._date_of_birth = dt.datetime.now()
         self._learning_time = learning_time
         self._model = model
+        self._mqtt = None
+        self._timer = None
+        #Flags...
+        self._is_active = True
 
     def get_name(self):
         return self._name
@@ -57,16 +63,23 @@ class Controller():
             self._mqtt.subscribe_to_topic(sensor, 1)
             self._mqtt.add_message_callback(
                 sensor, self.on_sensor_state_change)
+        #Set the Timer
+        self._timer = ResettableTimer(self._timeout, self.set_availability)
+        self._timer.start()
 
     def on_position_change(self, client, userdata, msg):
         print("Received Blind Position Change from topic: " +
               msg.topic + ' --> ' + msg.payload.decode())
         self._device.set_position_Blinds(msg.payload.decode())
+        self._timer.reset()
+        self._is_active = True
 
     def on_sensor_state_change(self, client, userdata, msg):
         print("Received Sensor State Change from topic: " +
               msg.topic + ' --> ' + msg.payload.decode())
         self._device.set_value_Sensor(msg.topic, msg.payload.decode())
+        self._timer.reset()
+        self._is_active = True
 
     def on_switch_state_change(self, client, userdata, msg):
         print("Received Switch State Change from topic: " +
@@ -76,13 +89,18 @@ class Controller():
         elif msg.payload.decode() == "ON":
             self._mqtt.publish_to_topic(
                 topic=self._device._switch.get_command_topic(), payload="OFF", qos=1)
+        self._timer.reset()
+        self._is_active = True
+    
+    def set_availability(self):
+        self._is_active = False
 
 
 def execution(device, credentials, run):
     control = Controller(device.get_name() + "_controller", device.get_id(), device)
     control.set_MQTTComponent(credentials["mqtt_host"], credentials["mqtt_user"], credentials["mqtt_passwd"])
     while True:
-        if run.value:
+        if run.value and control._is_active:
             print("Inside Process motherfucker!!")
             time.sleep(1)
         else:
